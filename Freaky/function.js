1f76017c98a9d36a4d5659266a7cc179769ccbb8
emden101 <ochinedu636@gmail.com>
2024-03-07 17:48:19 +0100

    First repository commits

---
+++ Freaky/function.js
@@ -0,0 +1,15 @@
+function addNumbers(number1, number2) {
+    var addition = number1 + number2;
+    return addition;
+}
+
+var result1 = addNumbers(12, 60);
+console.log(result1)
+
+function mathpow(num1, num2) {
+    var multiplez = num1 ** num2;
+    return multiplez;
+}
+
+var rez = mathpow(3, 7);
+console.log(rez);
